0c6bac8cd80c5d6ae671489add15f4d9bdb51427
Bastien <bastien.poinas@gmail.com>
2022-05-30 09:34:20 +0000

    first commit bastien

--- pays_modele.h
+++ pays_modele.h
@@ -1,2 +1,14 @@
 // Bastien
 // 
+
+
+#ifndef CELEBRITES_MODELE_H
+#define CELEBRITES_MODELE_H
+
+int celebrites_create( char *nom, char *prenom, char *genre );
+int celebrites_read(int celeb_id, char *nom, char *prenom, char *genre);
+int celebrites_update(int celeb_id, char *nom, char *prenom, char *genre);
+int celebrites_delete(int celeb_id);
+int celebrites_existe(int celeb_id);
+
+#endif
